518cfb05152764098cace86cf3632dbb320f560c
suryadhrma <suryamlg16@gmail.com>
2026-05-22 08:59:55 +0700

    perbaiki nama tab

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <title>@yield('title', 'MebelKita') - {{ config('app.name') }}</title>
+    <title>@yield('title', 'Beranda') - 541 Furniture</title>   
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
